13956ea34a803da019f855e53e2ce2339d5aeff5
faekhatami <faekhatami@gmail.com>
2024-07-06 16:19:28 +0330

    initial

---
+++ src/components/Header.test.js
@@ -0,0 +1,10 @@
+// src/components/Header.test.js
+import React from "react";
+import { render } from "@testing-library/react";
+import Header from "./Header";
+
+test("renders header correctly", () => {
+  const { getByText } = render(<Header />);
+  expect(getByText(/Home/i)).toBeInTheDocument();
+  expect(getByText(/Cart/i)).toBeInTheDocument();
+});
